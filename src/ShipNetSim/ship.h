ee457ee5c6c8a5d43b173bd82a4129210e96b232
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-08-11 11:35:43 -0400

    basic definition of the ship

--- src/ShipNetSim/ship.h
+++ src/ShipNetSim/ship.h
@@ -8,10 +8,101 @@ class Ship : public QObject
 {
     Q_OBJECT
 public:
+
+
+    /**
+     * @brief defines the Wet Surface Area Calculation Method
+     *
+     * @details
+     * Method 'Holtrop' predicts the wetted surface of the hull at rest
+     * if it is not knows from hydrostatic analysis.
+     * @cite  birk2019fundamentals <br>
+     * Method '':
+     */
+    enum class WetSurfaceAreaCalculationMethod {
+        Holtrop,
+        Schenzle,
+        Cargo,
+        Trawlers
+    };
+
+    /**
+     * @brief defines the Block Coefficient Methods
+     *
+     * @details
+     * Method 'Ayre': $C_B=C-1.68 F_n$ where often C = 1.06 is used. <br>
+     * Method 'Jensen': is recommended for modern ship hulls:
+     * $C_B=-4.22+27.8 \cdot \sqrt{F_n}-39.1 \cdot F_n+46.6 \cdot F_n^3$
+     * for $0.15<F_n<0.32$ <br>
+     * Method 'Schneekluth': is optimized aiming to reduce 'production costs’
+     * for specified deadweight and speed. The formula is valid for
+     * $0:48 <= CB <= 0:85$ and $0:14 <= Fn <= 0:32$. However, for actual
+     * $Fn >= 0.3$ only Fn = 0.30 is used.
+     * @cite schneekluth1998ship
+     */
+    enum class BlockCoefficientMethod {
+        Ayre,
+        Jensen,
+        Schneekluth
+    };
+
+    /**
+     * @brief defines the Water Plane Coefficient Methods
+     *
+     * @details
+     * Methods 'U_Shape', 'Average_Section', and 'V_Section' are only
+     * applicable to ships with cruiser sterns and ‘cut-away cruiser sterns’.
+     * As these formulae are not applicable to vessels with submerged
+     * transom sterns, they should be tested on a ‘similar ship’
+     * @cite schneekluth1998ship   <br>
+     * ---<br>
+     * Methods 'General_Cargo' and 'Container' gives only initial estimates.
+     * @cite birk2019fundamentals
+     */
+    enum class WaterPlaneCoefficientMethod {
+        U_Shape,
+        Average_Section,
+        V_Section,
+        Tanker_Bulker,
+        General_Cargo,
+        Container
+    };
+
+    // Declaration of a custom exception class within MyClass
+    class ShipException : public std::exception {
+    public:
+        ShipException(
+            const std::string& message) : msg_(message) {}
+        const char* what() const noexcept override {
+            return msg_.c_str();
+        }
+    private:
+        std::string msg_;
+    };
+
+
+
+
     Ship();
 
-    units::area::square_meter_t get_wet_surface_area_to_holtrop();
-    units::area::square_meter_t get_wet_surface_area_to_schenzle();
+
+    units::area::square_meter_t get_wet_surface_area(
+        WetSurfaceAreaCalculationMethod method =
+        WetSurfaceAreaCalculationMethod::Holtrop
+        );
+
+    double get_C_B(units::velocity::meters_per_second_t &speed,
+                   BlockCoefficientMethod method =
+                   BlockCoefficientMethod::Ayre);
+
+    double get_C_P();
+
+    double get_Nab();
+
+
+    double get_C_WP(WaterPlaneCoefficientMethod method =
+                    WaterPlaneCoefficientMethod::General_Cargo);
+
 
 private:
     units::length::meter_t L;
@@ -22,7 +113,7 @@ private:
     units::length::meter_t T_A;
     units::area::square_meter_t S;
     units::length::meter_t h_b;
-    units::volume::cubic_meter_t Nab;
+    units::volume::cubic_meter_t Nab; // volumetric displacement
     units::area::square_meter_t S_app;
     units::area::square_meter_t A_BT;
     units::angle::degree_t i_E;
@@ -60,8 +151,8 @@ private:
 
 
 
-
-
+    units::area::square_meter_t get_wet_surface_area_to_holtrop();
+    units::area::square_meter_t get_wet_surface_area_to_schenzle();
 
 };
 
