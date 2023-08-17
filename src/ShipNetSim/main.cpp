8e238e4107aff93f68c70aef2376abc1298b232e
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-08-17 12:18:12 -0400

    add the logger class

--- src/ShipNetSim/main.cpp
+++ src/ShipNetSim/main.cpp
@@ -1,8 +1,18 @@
 #include <QCoreApplication>
+#include "utils/logger.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    Logger::attach();
+
+
+
+
+
+
+    Logger::detach();
     return a.exec();
 }
