dee268e6ad829bedaf7fbb8f57d440d124240599
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-09 09:53:21 -0400

    init network point

---
+++ src/ShipNetSim/network/point.h
@@ -0,0 +1,48 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include "geometry.h"
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/register/point.hpp>
+#include <functional>
+
+class Point : public Geometry
+{
+public:
+    Point();
+    Point (long double xCoord,
+          long double yCoord,
+          std::string ID,
+          int index);
+    ~Point();
+    long double distance(Point &endPoint);
+    std::string toString() override;
+    bool operator==(const Point& other) const;
+//    bool operator>(const Point& other) const;
+//    bool operator<(const Point& other) const;
+
+    long double x;
+    long double y;
+
+private:
+    std::string userID;
+    int index;
+
+};
+
+BOOST_GEOMETRY_REGISTER_POINT_2D(Point, long double,
+                                 boost::geometry::cs::cartesian,
+                                 x,y)
+
+
+template <>
+struct std::hash<Point> {
+    std::size_t operator()(const Point& p) const;
+};
+
+//struct PointEqual {
+//    bool operator()(const Point& lhs, const Point& rhs) const;
+//};
+
+#endif // POINT_H
