c0a15d9795401c228c2f234dbecb00d53169cb3c
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-14 19:38:30 -0400

    add get coordinate on the line by distance

--- src/ShipNetSim/network/line.h
+++ src/ShipNetSim/network/line.h
@@ -1,23 +1,33 @@
 #ifndef LINE_H
 #define LINE_H
 
-#include "geometry.h"
+#include "basegeometry.h"
 #include "point.h"
+#include "../../third_party/units/units.h"
 
-class Line : public Geometry
+class Line : public BaseGeometry
 {
 private:
     boost::geometry::model::segment<Point> internal_segment;
+    long double mLength;
+    long double mMaxSpeed;
 
 public:
-    Line(const Point& start, const Point& end);
+    enum class LineEnd {
+        Start,
+        End
+    };
+    Line(const Point start, const Point end,
+         units::velocity::meters_per_second_t maxSpeed);
     ~Line();
     Point startPoint() const;
     Point endPoint() const;
     long double length() const;
-    bool intersects(const boost::geometry::model::polygon<Point>&
-                        polygon) const;
-
+    bool intersects(
+        const boost::geometry::model::polygon<Point>& polygon) const;
+    long double angleWith(const Line& other) const;
+    Point getPointByDistance(long double distance, LineEnd from) const;
+    Point getPointByDistance(long double distance, Point &from) const;
     std::string toString() override;
 };
 
