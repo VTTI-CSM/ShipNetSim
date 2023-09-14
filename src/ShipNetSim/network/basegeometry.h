5fddfb6d9f8a2a66685c1766f03dff30a5f29edb
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-14 19:35:44 -0400

    base class for any geometry class

---
+++ src/ShipNetSim/network/basegeometry.h
@@ -0,0 +1,13 @@
+#ifndef BASEGEOMETRY_H
+#define BASEGEOMETRY_H
+
+#include <stdlib.h>
+#include <string>
+
+class BaseGeometry
+{
+public:
+    virtual std::string toString() = 0;
+};
+
+#endif // BASEGEOMETRY_H
