e206874f5d92b4f1e1ffb078e8f9c8170d941a7a
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-14 19:41:15 -0400

    define max speed within the polygon

--- src/ShipNetSim/network/polygon.cpp
+++ src/ShipNetSim/network/polygon.cpp
@@ -11,6 +11,41 @@ Polygon::Polygon(const std::vector<Point>& boundary,
         boost::geometry::assign_points(internal_polygon.inners().back(), hole);
     }
 }
+void Polygon::setMaxAllowedSpeed(
+    const units::velocity::meters_per_second_t newMaxSpeed)
+{
+    mMaxSpeed = newMaxSpeed;
+}
+units::velocity::meters_per_second_t Polygon::getMaxAllowedSpeed()
+{
+    return mMaxSpeed;
+}
+
+bool Polygon::pointIsInPolygon(const Point &pointToCheck)
+{
+    // Check vertices in the outer ring
+    for (const auto& vertex : internal_polygon.outer())
+    {
+        if (vertex == pointToCheck)
+        {
+            return true;
+        }
+    }
+
+    // Check vertices in the inner rings (if any)
+    for (const auto& inner_ring : internal_polygon.inners())
+    {
+        for (const auto& vertex : inner_ring)
+        {
+            if (vertex == pointToCheck)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
 long double Polygon::area() const {
     return boost::geometry::area(internal_polygon);
--- src/ShipNetSim/network/polygon.h
+++ src/ShipNetSim/network/polygon.h
@@ -1,19 +1,28 @@
 #ifndef POLYGON_H
 #define POLYGON_H
 
-#include "geometry.h"
+#include "basegeometry.h"
 #include "Point.h"
 #include <vector>
 #include <boost/geometry.hpp>
+#include "../../third_party/units/units.h"
 
-class Polygon : Geometry
+class Polygon : BaseGeometry
 {
+private:
+    units::velocity::meters_per_second_t mMaxSpeed =
+        units::velocity::meters_per_second_t(200.0);
+
 public:
     boost::geometry::model::polygon<Point> internal_polygon;
 
     Polygon();
     Polygon(const std::vector<Point>& boundary,
             const std::vector<std::vector<Point>>& holes = {});
+    void setMaxAllowedSpeed(
+        const units::velocity::meters_per_second_t newMaxSpeed);
+    units::velocity::meters_per_second_t getMaxAllowedSpeed();
+    bool pointIsInPolygon(const Point& pointToCheck);
 
     long double area() const;
     long double perimeter() const;
