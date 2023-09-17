a0f2bfce37f72fa56ba4a112bf609224c5df4884
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-16 21:25:53 -0400

    get home directory

--- src/ShipNetSim/utils/utils.h
+++ src/ShipNetSim/utils/utils.h
@@ -4,6 +4,7 @@
 #include <any>
 #include <QString>
 #include <QMap>
+#include <QDir>
 
 namespace Utils
 {
@@ -13,6 +14,31 @@ namespace Utils
     {
         return parameters.contains(key) ?
                    std::any_cast<T>(parameters[key]) : defaultValue;
+    }
+
+    inline QString getHomeDirectory()
+    {
+        QString homeDir;
+
+        // OS-dependent home directory retrieval
+#if defined(Q_OS_WIN)
+        homeDir = QDir::homePath();
+#elif defined(Q_OS_LINUX) || defined(Q_OS_MAC)
+        homeDir = QStandardPaths::writableLocation(QStandardPaths::HomeLocation);
+#endif
+
+        if (!homeDir.isEmpty())
+        {
+            // Creating a path to NeTrainSim folder in the Documents directory
+            const QString documentsDir = QDir(homeDir).filePath("Documents");
+            const QString folder = QDir(documentsDir).filePath("ShipNetSim");
+
+            QDir().mkpath(folder); // Create the directory if it doesn't exist
+
+            return folder;
+        }
+
+        throw std::runtime_error("Error: Cannot retrieve home directory!");
     }
 }
 #endif // UTILS_H
