31bed27438268acea6238615cbb23aa95a44223f
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-09-16 21:25:21 -0400

    read TXT file

--- src/ShipNetSim/utils/data.cpp
+++ src/ShipNetSim/utils/data.cpp
@@ -22,36 +22,74 @@ std::vector<T> Data::Table::getColumn(const std::string& headerName) const
     throw std::runtime_error("Header not found: " + headerName);
 }
 
-Data::Table Data::CSV::read(const QString& filePath,
-                         const std::vector<std::string>& typeSequence,
-                         const std::string& separator)
-{
-    Table table;
-    QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-    {
-        throw std::runtime_error("Could not open file: " +
-                                 filePath.toStdString());
+Data::CSV::CSV() : mFilePath("") {}
+
+Data::CSV::CSV(const QString &filePath) :
+    mFilePath(filePath), mFile(filePath) {}
+
+Data::CSV::~CSV() {
+    if(mFile.isOpen()) {
+        mFile.close();
+    }
+}
+
+void Data::CSV::initCSV(const QString &filePath) {
+    mFilePath = filePath;
+    mFile.setFileName(mFilePath);
+}
+
+bool Data::CSV::writeLine(const std::string &line) {
+    if(!mFile.isOpen()) {
+        if (!mFile.open(QIODevice::WriteOnly |
+                        QIODevice::Text |
+                        QIODevice::Append)) {
+            return false;
+        }
+        mOutStream.setDevice(&mFile);
     }
 
-    QTextStream in(&file);
-    bool firstLine = true;
+    mOutStream << QString::fromStdString(line) << "\n";
+    return true;
+}
+
+Data::Table Data::CSV::read(const std::vector<std::string>& typeSequence,
+                            const bool hasHeaders,
+                            const std::string& separator)
+{
+    Table table;
+
+    if (!mFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        throw std::runtime_error("Could not open file: " +
+                                 mFilePath.toStdString());
+    }
+
+    QTextStream in(&mFile);
+
+    if (hasHeaders) {
+        QString headerLine = in.readLine();
+        std::string headerStr = headerLine.toStdString();
+
+        boost::split(table.headers,
+                     headerStr,
+                     boost::is_any_of(separator));
+
+        for (const auto& header : table.headers) {
+            table.tableMap[header] = std::vector<Cell>();
+        }
+    }
+    else
+    {
+        for (std::size_t i = 0; i < typeSequence.size(); ++i) {
+            std::string header = "Column" + std::to_string(i);
+            table.headers.push_back(header);
+            table.tableMap[header] = std::vector<Cell>();
+        }
+    }
+
     while (!in.atEnd()) {
         QString line = in.readLine();
         std::string lineStr = line.toStdString();
-
-        if (firstLine) {
-            boost::split(table.headers,
-                         lineStr,
-                         boost::is_any_of(separator));
-
-            for (const auto& header : table.headers)
-            {
-                table.tableMap[header] = std::vector<Cell>();
-            }
-            firstLine = false;
-            continue;
-        }
 
         std::vector<std::string> rowStr;
         boost::split(rowStr,
@@ -60,47 +98,46 @@ Data::Table Data::CSV::read(const QString& filePath,
 
         if (rowStr.size() != typeSequence.size()) {
             throw std::runtime_error(
-                "Number of columns does not "
-                "match the provided type sequence");
+                "Number of columns does not match "
+                "the provided type sequence");
         }
 
         for (std::size_t i = 0; i < table.headers.size(); ++i) {
-            if (typeSequence[i] == "int")
-            {
+            if (typeSequence[i] == "int") {
                 table.tableMap[table.headers[i]].
                     push_back(std::stoi(rowStr[i]));
             }
-            else if (typeSequence[i] == "double")
-            {
+            else if (typeSequence[i] == "double") {
                 table.tableMap[table.headers[i]].
                     push_back(std::stod(rowStr[i]));
             }
-            else if (typeSequence[i] == "string")
-            {
+            else if (typeSequence[i] == "string") {
                 table.tableMap[table.headers[i]].
                     push_back(rowStr[i]);
             }
-            else
-            {
+            else {
                 throw std::runtime_error(
                     "Unknown data type in type sequence: " +
                     typeSequence[i]);
             }
         }
     }
+
+    // Close the file after reading
+    mFile.close();
+
     return table;
 }
 
 
-Data::Table Data::CSV::filterTable(
-    const Table& originalTable,
+Data::Table Data::Table::filterTable(
     const std::string& columnName,
     std::function<bool(const Cell&)> filterFunction)
 {
     Table filteredTable;
 
     // Copy the headers
-    filteredTable.headers = originalTable.headers;
+    filteredTable.headers = headers;
 
     // Initialize the columns in filteredTable
     for (const auto& header : filteredTable.headers) {
@@ -108,29 +145,123 @@ Data::Table Data::CSV::filterTable(
     }
 
     // Check if column exists in the original table
-    if (originalTable.tableMap.find(columnName) ==
-        originalTable.tableMap.end()) {
+    if (tableMap.find(columnName) == tableMap.end())
+    {
         throw std::runtime_error("Column not found: "
                                  + columnName);
     }
 
     // Loop through rows to apply the filter
-    std::size_t numRows =
-        originalTable.tableMap.begin()->second.size();
+    std::size_t numRows = tableMap.begin()->second.size();
 
     for (std::size_t i = 0; i < numRows; ++i)
     {
         // Apply filter function
-        if (filterFunction(originalTable.tableMap.at(columnName)[i]))
+        if (filterFunction(tableMap.at(columnName)[i]))
         {
             // Copy this row to filteredTable
             for (const auto& header : filteredTable.headers)
             {
                 filteredTable.tableMap[header].
-                    push_back(originalTable.tableMap.at(header)[i]);
+                    push_back(tableMap.at(header)[i]);
             }
         }
     }
 
     return filteredTable;
+}
+
+Data::TXT::TXT() : mFilePath("") {}
+
+Data::TXT::TXT(const QString &filePath) :
+    mFilePath(filePath), mFile(filePath) {}
+
+Data::TXT::~TXT() {
+    if(mFile.isOpen()) {
+        mFile.close();
+    }
+}
+
+void Data::TXT::initTXT(const QString &filePath) {
+    mFilePath = filePath;
+    mFile.setFileName(mFilePath);
+}
+
+Data::Table Data::TXT::read(const std::vector<std::string>& typeSequence,
+                            const std::string& separator)
+{
+    Table table;
+
+    if (!mFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        throw std::runtime_error("Could not open file: " +
+                                 mFilePath.toStdString());
+    }
+
+    QTextStream in(&mFile);
+
+    for (std::size_t i = 0; i < typeSequence.size(); ++i) {
+        std::string header = "Column" + std::to_string(i);
+        table.headers.push_back(header);
+        table.tableMap[header] = std::vector<Cell>();
+    }
+
+
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        std::string lineStr = line.toStdString();
+
+        std::vector<std::string> rowStr;
+        boost::split(rowStr,
+                     lineStr,
+                     boost::is_any_of(separator));
+
+        if (rowStr.size() != typeSequence.size()) {
+            throw std::runtime_error(
+                "Number of columns does not match "
+                "the provided type sequence");
+        }
+
+        for (std::size_t i = 0; i < table.headers.size(); ++i) {
+            if (typeSequence[i] == "int") {
+                table.tableMap[table.headers[i]].
+                    push_back(std::stoi(rowStr[i]));
+            }
+            else if (typeSequence[i] == "double") {
+                table.tableMap[table.headers[i]].
+                    push_back(std::stod(rowStr[i]));
+            }
+            else if (typeSequence[i] == "string") {
+                table.tableMap[table.headers[i]].
+                    push_back(rowStr[i]);
+            }
+            else {
+                throw std::runtime_error(
+                    "Unknown data type in type sequence: " +
+                    typeSequence[i]);
+            }
+        }
+    }
+
+    // Close the file after reading
+    mFile.close();
+
+    return table;
+}
+
+bool Data::TXT::writeFile(std::string &data)
+{
+    if(!mFile.isOpen()) {
+        if (!mFile.open(QIODevice::WriteOnly |
+                        QIODevice::Text |
+                        QIODevice::Append)) {
+            return false;
+        }
+        mOutStream.setDevice(&mFile);
+    }
+
+    mOutStream << QString::fromStdString(data) << "\n";
+    mFile.close();
+
+    return true;
 }
--- src/ShipNetSim/utils/data.h
+++ src/ShipNetSim/utils/data.h
@@ -1,12 +1,15 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <fstream>
 #include <vector>
 #include <map>
 #include <variant>
 #include <string>
 #include <stdexcept>
 #include <QString>
+#include <QFile>
+#include <QTextStream>
 
 using Cell = std::variant<int, double, std::string>;
 
@@ -18,48 +21,163 @@ class Table
 public:
     std::vector<std::string> headers;
     // this is effecient since i will need to loop over the whole ds anyways
-    // and i do not know the size of the table
+    // and we do not know the size of the table
     std::map<std::string, std::vector<Cell>> tableMap;
 
     // Templated function to access a column by its header name
     template <typename T>
     std::vector<T> getColumn(const std::string& headerName) const;
+
+    /**
+     * @brief CsvReader::filterTable
+     * @param originalTable
+     * @param columnName
+     * @param filterFunction
+     * @return
+     * @code
+     *    CsvTable filteredTable = reader.filterTable(table, "age",
+     *                                          [](const Cell& cell) -> bool {
+     *      if (cell.type() == typeid(int)) {
+     *          return boost::get<int>(cell) > 30;
+     *      }
+     *      return false;
+     *  });
+     */
+    inline Table filterTable(const std::string& columnName,
+                             std::function<bool(const Cell&)> filterFunction);
+
+    // Nested iterator class
+    class iterator
+    {
+    public:
+        using inner_iterator = std::map<std::string,
+                                        std::vector<Cell>>::iterator;
+
+        iterator(inner_iterator it) : it_(it) {}
+
+        iterator& operator++() {
+            ++it_;
+            return *this;
+        }
+
+        std::pair<const std::string, std::vector<Cell>>& operator*()
+        {
+            return *it_;
+        }
+
+        bool operator==(const iterator& other) const
+        {
+            return it_ == other.it_;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return it_ != other.it_;
+        }
+
+    private:
+        inner_iterator it_;
+    };
+
+    // Nested const_iterator class
+    class const_iterator {
+    public:
+        using inner_const_iterator =
+            std::map<std::string,
+                     std::vector<Cell>>::const_iterator;
+
+        const_iterator(inner_const_iterator it) : it_(it) {}
+
+        const_iterator& operator++()
+        {
+            ++it_;
+            return *this;
+        }
+
+        const std::pair<const std::string,
+                        std::vector<Cell>>& operator*() const
+        {
+            return *it_;
+        }
+
+        bool operator==(const const_iterator& other) const
+        {
+            return it_ == other.it_;
+        }
+
+        bool operator!=(const const_iterator& other) const
+        {
+            return it_ != other.it_;
+        }
+
+    private:
+        inner_const_iterator it_;
+    };
+
+    iterator begin()
+    {
+        return iterator(tableMap.begin());
+    }
+
+    iterator end() {
+        return iterator(tableMap.end());
+    }
+
+    const_iterator begin() const
+    {
+        return const_iterator(tableMap.cbegin());
+    }
+
+    const_iterator end() const
+    {
+        return const_iterator(tableMap.cend());
+    }
+
+    const_iterator cbegin() const
+    {
+        return const_iterator(tableMap.cbegin());
+    }
+
+    const_iterator cend() const
+    {
+        return const_iterator(tableMap.cend());
+    }
+
 };
 
-namespace CSV
+class CSV {
+private:
+    QString mFilePath;
+    QFile mFile;
+    QTextStream mOutStream; // For writing
+
+public:
+    CSV();
+    CSV(const QString &filePath);
+    ~CSV(); // Destructor
+    void initCSV(const QString &filePath);
+    Table read(const std::vector<std::string>& typeSequence,
+               const bool hasHeaders = false,
+               const std::string& separator = ",");
+    bool writeLine(const std::string &line);
+};
+
+class TXT
 {
+private:
+    QString mFilePath;
+    QFile mFile;
+    QTextStream mOutStream; // For writing
 
-/**
- * @brief read
- * @param filePath
- * @param typeSequence
- * @param separator
- * @return
- */
-inline Table read(const QString& filePath,
-                  const std::vector<std::string>& typeSequence,
-                  const std::string& separator = ",");
-
-/**
- * @brief CsvReader::filterTable
- * @param originalTable
- * @param columnName
- * @param filterFunction
- * @return
- * @code
- *    CsvTable filteredTable = reader.filterTable(table, "age",
- *                                          [](const Cell& cell) -> bool {
- *      if (cell.type() == typeid(int)) {
- *          return boost::get<int>(cell) > 30;
- *      }
- *      return false;
- *  });
- */
-inline Table filterTable(const Table& originalTable,
-                         const std::string& columnName,
-                         std::function<bool(const Cell&)> filterFunction);
+public:
+    TXT();
+    TXT(const QString &filePath);
+    ~TXT(); // Destructor to close the file
+    void initTXT(const QString &filePath);
+    inline Table read(const std::vector<std::string>& typeSequence,
+                      const std::string& separator = ",");
+    inline bool writeFile(std::string &data);
 };
-
 
 }
 
