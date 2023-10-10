b1ac351722ab8457c146854f52f0e727e7a83d9c
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-10-09 21:10:36 -0400

    define the ship propeller class

--- src/ShipNetSim/ship/shippropeller.cpp
+++ src/ShipNetSim/ship/shippropeller.cpp
@@ -5,94 +5,60 @@
 #include "../utils/utils.h"
 
 
-ShipPropeller::ShipPropeller()
-{
-    mHost = nullptr;
-}
-
-void ShipPropeller::initialize(Ship *ship,
+void ShipPropeller::initialize(Ship *ship, IShipGearBox *gearbox,
                                QMap<QString, std::any> &parameters)
 {
     mHost = ship;
-
-    mBreakPower =
-        Utils::getValueFromMap<units::power::kilowatt_t>(
-            parameters, "breakPower",
-            units::power::kilowatt_t(std::nan("uninitialized")));
-
-    mGearEfficiency =
-        Utils::getValueFromMap<double>(
-            parameters, "gearEfficiency", std::nan("uninitialized"));
-
-    mShaftEfficiency =
-        Utils::getValueFromMap<double>(
-            parameters, "shaftEfficiency", std::nan("uninitialized"));
-
-    mPropellerEfficiency =
-        Utils::getValueFromMap<double>(
-            parameters, "propellerEfficiency", std::nan("uninitialized"));
-
-    mHullEfficiency =
-        Utils::getValueFromMap<double>(
-            parameters, "hullEfficiency", std::nan("uninitialized"));
-
-    initializeDefaults();
+    mGearBox = gearbox;
+    setParameters(parameters);
 
 }
 
-void ShipPropeller::setHost(Ship *ship)
-{
-    mHost = ship;
-}
 
 void ShipPropeller::setParameters(QMap<QString, std::any> &parameters)
 {
-    if (parameters.contains("breakPower"))
+    // Process Shaft Efficiency
+    auto it = parameters.find("shaftefficiency");
+    if (it != parameters.end())
     {
-        mBreakPower =
-            std::any_cast<units::power::kilowatt_t>(parameters["breakPower"]);
+        mShaftEfficiency = std::any_cast<double>(it.value());
     }
-    if (parameters.contains("gearEfficiency"))
+
+    // Process Open Water Efficiency
+    it = parameters.find("openwaterefficiency");
+    if (it != parameters.end())
     {
-        mGearEfficiency =
-            std::any_cast<double>(parameters["gearEfficiency"]);
+        mPropellerOpenWaterEfficiencyToJ =
+            std::any_cast<QMap<double, double>>(it.value());
     }
-    if (parameters.contains("shaftEfficiency"))
+
+    // Process Propeller Diameter & Disk Area
+    it = parameters.find("propellerdiameter");
+    if (it != parameters.end())
     {
-        mShaftEfficiency =
-            std::any_cast<double>(parameters["shaftEfficiency"]);
+        mPropellerDiameter =
+            std::any_cast<units::length::meter_t>(it.value());
+        mPropellerDiskArea =
+            units::constants::pi *
+                             units::math::pow<2>(mPropellerDiameter) / 4.0;
     }
-    if (parameters.contains("propellerEfficiency"))
+
+    // Process Expanded Area Ratio & Blade Area
+    it = parameters.find("propellerExpandedAreaRatio");
+    if (it != parameters.end())
     {
-        mPropellerEfficiency =
-            std::any_cast<double>(parameters["propellerEfficiency"]);
-    }
-    if (parameters.contains("hullEfficiency"))
-    {
-        mHullEfficiency =
-            std::any_cast<double>(parameters["hullEfficiency"]);
+        mPropellerExpandedAreaRatio =
+            std::any_cast<double>(it.value());
+        mExpandedBladeArea =
+            mPropellerExpandedAreaRatio * mPropellerDiskArea;
     }
 }
 
-ShipPropeller::~ShipPropeller()
-{
-    if (mHost)
-    {
-        delete mHost;
-    }
-}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~ Getters and Setters ~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-double ShipPropeller::getGearEfficiency()
-{
-    return mGearEfficiency;
-}
-void ShipPropeller::setGearEfficiency(double newGearEfficiency)
-{
-    mGearEfficiency = newGearEfficiency;
-}
+
 
 double ShipPropeller::getShaftEfficiency()
 {
@@ -106,54 +72,27 @@ void ShipPropeller::setShaftEfficiency(double newShaftEfficiency)
 
 double ShipPropeller::getPropellerEfficiency()
 {
-    return mPropellerEfficiency;
+    return getOpenWaterEfficiency() * getRelativeEfficiency();
 }
 
-void ShipPropeller::setPropellerEfficiency(double newPropellerEfficiency)
+void ShipPropeller::setPropellerOpenWaterEfficiencies(
+    QMap<double, double> newPropellerOpenWaterEfficiencies)
 {
-    mPropellerEfficiency = newPropellerEfficiency;
-}
-
-double ShipPropeller::getHullEfficiency()
-{
-    return mHullEfficiency;
-}
-
-void ShipPropeller::setHullEfficiency(double newHullEfficiency)
-{
-    mHullEfficiency = newHullEfficiency;
-}
-
-
-units::power::kilowatt_t ShipPropeller::getBreakPower()
-{
-    return mBreakPower;
-}
-
-void ShipPropeller::setBreakPower(units::power::kilowatt_t newPower)
-{
-    mBreakPower = newPower;
-}
-
-units::power::kilowatt_t ShipPropeller::getShaftPower()
-{
-    return getBreakPower() * mGearEfficiency;
-}
-
-
-units::power::kilowatt_t ShipPropeller::getDeliveredPower()
-{
-    return getShaftPower() * mShaftEfficiency;
-}
-
-units::power::kilowatt_t ShipPropeller::getThrustPower()
-{
-    return getDeliveredPower() * mPropellerEfficiency;
+    mPropellerOpenWaterEfficiencyToJ = newPropellerOpenWaterEfficiencies;
 }
 
 units::power::kilowatt_t ShipPropeller::getEffectivePower()
 {
-    return getThrustPower() * mHullEfficiency;
+    mPreviousEffectivePower = mGearBox->getOutputPower() *
+           getPropellerEfficiency() *
+           mShaftEfficiency *
+           getHullEfficiency();
+    return mPreviousEffectivePower;
+}
+
+units::power::kilowatt_t ShipPropeller::getPreviousEffectivePower()
+{
+    return mPreviousEffectivePower;
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -162,97 +101,67 @@ units::power::kilowatt_t ShipPropeller::getEffectivePower()
 
 units::force::newton_t ShipPropeller::getThrust()
 {
-    return units::math::min(
-        (getEffectivePower() /
-         units::math::max(
-             mHost->getResistanceStrategy()->calc_SpeedOfAdvance(*mHost),
-             units::velocity::meters_per_second_t(0.0001))).
-        convert<units::force::newton>(),
-
-        units::force::kilonewton_t(
-            (double)2.0 * hydrology::WATER_RHO.value() *
-            mHost->getPropellerDiskArea().value() *
-            pow(getDeliveredPower().convert<units::power::watts>().value(),
-                (double)2.0) / (double)1000.0
-            ).convert<units::force::newton>());
+    // 70% of the raduis only provide thrust
+    return getEffectivePower()/mHost->getResistanceStrategy()->
+                                 calc_SpeedOfAdvance(*mHost);
 }
 
-double ShipPropeller::getRPM()
+units::angular_velocity::revolutions_per_minute_t ShipPropeller::getRPM()
 {
-    return pow(pow(getShaftPower().
-                   convert<units::power::horsepower>().value(),
-                   (double)0.2) * ((double)632.7/
-                      mHost->getPropellerDiameter().
-                      convert<units::length::inch>().value()),
-               ((double)1 / (double)0.6));
+    return mGearBox->getOutputRPM();
 }
 
 units::torque::newton_meter_t ShipPropeller::getTorque()
 {
     return units::torque::newton_meter_t(
-        getDeliveredPower().convert<units::power::watt>().value() /
-        ((double)2.0 * units::constants::pi.value() * getRPM()));
-
-    //    return units::torque::foot_pound_t(
-    //               getEffectivePower().convert<units::power::horsepower>().value() /
-    //                                       (getRPM() / (double)5252.0)).
-    //        convert<units::torque::newton_meter>();
+        getEffectivePower().convert<units::power::watt>().value() /
+        getRPM().convert<units::angular_velocity::
+                         radians_per_second>().value());
 }
-
-//units::force::newton_t ShipPropeller::calc_Torque()
-//{
-//    return units::force::kilonewton_t(
-//               cbrt((double)2.0 *
-//                    hydrology::WATER_RHO.value() *
-//                    mHost->getPropellerDiskArea().value() *
-//                    pow(getDeliveredPower().
-//                        convert<units::power::watts>().value(), (double)2.0)
-//                    )/(double)1000.0).convert<units::force::newton>();
-//}
 
 double ShipPropeller::getThrustCoefficient()
 {
     return (getThrust().value() /
             (hydrology::WATER_RHO.value() *
-             pow(getRPM(), (double)2.0) *
-             pow(mHost->getPropellerDiameter().value(),(double)4.0)));
+             pow(getRPM().value(), (double)2.0) *
+             pow(getPropellerDiameter().value(),(double)4.0)));
 }
 
 double ShipPropeller::getTorqueCoefficient()
 {
     return (getTorque().value() /
             (hydrology::WATER_RHO.value() *
-             pow(getRPM(), (double)2.0) *
-             pow(mHost->getPropellerDiameter().value(),(double)5.0)));
+             pow(getRPM().value(), (double)2.0) *
+             pow(getPropellerDiameter().value(),(double)5.0)));
 }
 
 double ShipPropeller::getAdvancedRatio()
 {
     return (mHost->getResistanceStrategy()->
             calc_SpeedOfAdvance(*mHost).value() /
-            (getRPM() * mHost->getPropellerDiameter().value()));
+            (getRPM().value() * getPropellerDiameter().value()));
 }
 
-void ShipPropeller::initializeDefaults()
+const QVector<IShipEngine *> ShipPropeller::getDrivingEngines() const
 {
-    if (std::isnan(mGearEfficiency))
-    {
-        mGearEfficiency = (double)0.99;
-    }
+    return mGearBox->getEngines();
+}
 
-    if (std::isnan(mShaftEfficiency))
-    {
-        mShaftEfficiency = (double)0.00;
-    }
+double ShipPropeller::getHullEfficiency()
+{
+    return mHost->getResistanceStrategy()->getHullEffeciency(*mHost);
+}
 
-    if (std::isnan(mPropellerEfficiency))
-    {
-        mPropellerEfficiency = (double)0.75;
-    }
+double ShipPropeller::getRelativeEfficiency()
+{
+    return mHost->getResistanceStrategy()->
+        getPropellerRotationEfficiency(*mHost);
 
-    if (std::isnan(mHullEfficiency))
-    {
-        mHullEfficiency = 0.99;
-    }
+}
 
+double ShipPropeller::getOpenWaterEfficiency()
+{
+    return Utils::interpolate(mPropellerOpenWaterEfficiencyToJ,
+                              mHost->getResistanceStrategy()->
+                              calc_SpeedOfAdvance(*mHost).value());
 }
