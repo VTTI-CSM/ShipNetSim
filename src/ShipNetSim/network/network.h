f436390d821486a4d122a0a170952fddfbd56d23
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-10-09 20:54:13 -0400

    add region name

--- src/ShipNetSim/network/network.h
+++ src/ShipNetSim/network/network.h
@@ -3,29 +3,29 @@
 
 
 #include "polygon.h"
-#include "../ship/ship.h"
 #include "visibilitygraph.h"
-class Network
+#include "QObject"
+
+class Ship;
+
+class Network : QObject
 {
+    Q_OBJECT
+
 private:
     std::shared_ptr<Polygon> mWaterBoundries;
     std::shared_ptr<VisibilityGraph> mVisibilityGraph;
-    std::string mRegionName;
+    QString mRegionName;
 public:
     Network();
     Network(std::shared_ptr<Polygon> waterBoundries,
-            std::string regionName = "");
+            QString regionName = "");
     ~Network();
     void seWaterBoundries(std::shared_ptr<Polygon> waterBoundries);
-    void setRegionName(std::string newName);
+    QString getRegionkName();
+    void setRegionName(QString newName);
     ShortestPathResult dijkstraShortestPath(std::shared_ptr<Point> startPoint,
                                              std::shared_ptr<Point> endpoint);
-
-    std::vector<units::length::meter_t>
-    generateCumLinesLengths(std::shared_ptr<Ship> ship);
-
-    long double getTotalPathLength(std::shared_ptr<Ship> ship);
-    Point getPositionByTravelledDistance(std::shared_ptr<Ship> ship);
 
 };
 
