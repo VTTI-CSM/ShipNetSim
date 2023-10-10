d928beda95702278eb808136bc4daa0ca414b072
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-10-09 21:14:22 -0400

    define revolution per second

--- src/third_party/units/units.h
+++ src/third_party/units/units.h
@@ -3635,6 +3635,7 @@ namespace units
 	UNIT_ADD(angular_velocity, radians_per_second, radians_per_second, rad_per_s, unit<std::ratio<1>, units::category::angular_velocity_unit>)
 	UNIT_ADD(angular_velocity, degrees_per_second, degrees_per_second, deg_per_s, compound_unit<angle::degrees, inverse<time::seconds>>)
 	UNIT_ADD(angular_velocity, revolutions_per_minute, revolutions_per_minute, rpm, unit<std::ratio<2, 60>, radians_per_second, std::ratio<1>>)
+	UNIT_ADD(angular_velocity, revolutions_per_second, revolutions_per_second, rps, unit<std::ratio<2, 1>, radians_per_second, std::ratio<1>>)
 	UNIT_ADD(angular_velocity, milliarcseconds_per_year, milliarcseconds_per_year, mas_per_yr, compound_unit<angle::milliarcseconds, inverse<time::year>>)
 
 	UNIT_ADD_CATEGORY_TRAIT(angular_velocity)
