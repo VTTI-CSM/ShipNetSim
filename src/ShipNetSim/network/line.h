4fcd4cd3f2d7d0f5ad7505a3ac91597cf9ae12ac
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2023-10-09 20:53:33 -0400

    fix a bug in calculating perpendiculat distance

--- src/ShipNetSim/network/line.h
+++ src/ShipNetSim/network/line.h
@@ -4,6 +4,7 @@
 #include "basegeometry.h"
 #include "point.h"
 #include "../../third_party/units/units.h"
+#include "algebraicvector.h"
 #include <cmath>
 #include <memory>
 
@@ -14,6 +15,8 @@ private:
     std::shared_ptr<Point> end;
     units::length::meter_t mLength;
     units::velocity::meters_per_second_t mMaxSpeed;
+    units::length::meter_t mWidth;
+    units::length::meter_t mDepth;
 
 public:
     enum class LineEnd {
@@ -27,23 +30,38 @@ public:
         CounterClockwise
     };
 
+    enum LocationToLine
+    {
+        left,
+        right,
+        onLine
+    };
+
     Line(std::shared_ptr<Point> start, std::shared_ptr<Point> end,
          units::velocity::meters_per_second_t maxSpeed);
-    Line(std::shared_ptr<Point> start, std::shared_ptr<Point> end);
+    Line(std::shared_ptr<Point> start,
+         std::shared_ptr<Point> end);
     ~Line();
     std::shared_ptr<Point> startPoint() const;
     std::shared_ptr<Point> endPoint() const;
     units::length::meter_t length() const;
+    units::velocity::meters_per_second_t getMaxSpeed() const;
+    units::length::meter_t depth() const;
     bool intersects(Line& other) const;
     units::angle::radian_t angleWith(Line& other) const;
     Point getPointByDistance(units::length::meter_t distance,
                              LineEnd from) const;
     Point getPointByDistance(units::length::meter_t distance,
                              std::shared_ptr<Point> from) const;
-    units::length::meter_t getPerpendicularDistance(
+    units::length::meter_t getPerpendicularDistance(const Point& point) const;
+    units::length::meter_t getTheoriticalWidth() const;
+    void setTheoriticalWidth(const units::length::meter_t newWidth);
+    AlgebraicVector toAlgebraicVector(
+        const std::shared_ptr<Point> startPoint) const;
+    LocationToLine getlocationToLine(
         const std::shared_ptr<Point>& point) const;
     bool operator==(const Line& other) const;
-    std::string toString() override;
+    QString toString() override;
 };
 
 #endif // LINE_H
