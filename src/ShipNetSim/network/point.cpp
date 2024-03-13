c89e619b5a4003106504120a1e6e346c188d2442
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-03-12 22:18:20 -0400

    add a feature to support Projected Coordinate System

--- src/ShipNetSim/network/point.cpp
+++ src/ShipNetSim/network/point.cpp
@@ -13,16 +13,18 @@
  */
 
 #include "point.h"  // Include the header file for the Point class.
+#include "gpoint.h" // Include the header file for the Geodetic point class
 #include <cmath>  // Include the cmath library for mathematical functions.
-#include <sstream>  // Include for string stream operations.
+// #include <sstream>  // Include for string stream operations.
+#include <QtEndian>  // Include Qt's endianness conversion functions
+#include <GeographicLib/Geocentric.hpp>
 
+std::shared_ptr<OGRSpatialReference> Point::spatialRef = nullptr;
 
 // Default constructor, initializes members with NaN and default values.
 Point::Point()
-    : mx(std::nan("No Value")),  // Initialize mx with NaN.
-    my(std::nan("No Value")),  // Initialize my with NaN.
-    userID(""),  // Initialize userID with an empty string.
-    index(0),  // Initialize index with 0.
+    : mOGRPoint(std::nan("No Value"), std::nan("No Value")), // Initialize NaN.
+    mUserID(""),  // Initialize userID with an empty string.
     mIsPort(false),  // Initialize mIsPort with false.
     mDwellTime(units::time::second_t(0.0))  // Initialize mDwellTime with 0.
 {}
@@ -30,26 +32,177 @@ Point::Point()
 // Parameterized constructor with user ID and index specified.
 Point::Point(units::length::meter_t xCoord,
              units::length::meter_t yCoord,
-             QString ID, unsigned int index)
-    : mx(xCoord),  // Initialize mx with xCoord.
-    my(yCoord),  // Initialize my with yCoord.
-    mIsPort(false),  // Initialize mIsPort with false.
-    mDwellTime(units::time::second_t(0.0)),  // Initialize mDwellTime with 0.
-    userID(ID),  // Initialize userID with ID.
-    index(index)  // Initialize index with index.
-{}
+             QString ID,
+             OGRSpatialReference crc)
+    : mOGRPoint(xCoord.value(), yCoord.value()),  // Initialize the Coord.
+    mUserID(ID),  // Initialize mIsPort with false.
+    mIsPort(false),  // Initialize userID with ID.
+    mDwellTime(units::time::second_t(0.0))  // Initialize index with index.
+{
+    OGRSpatialReference* SR = nullptr;
+    if (!crc.IsEmpty()) {
+        if (!crc.IsProjected())
+        {
+            qFatal("Spatial reference must be projected!");
+        }
+        // Clone the passed spatial reference and assign
+        // it to spatialRef pointer
+        SR = crc.Clone();
+    } else {
+        // If no spatial reference is passed, create a
+        // new spatial reference for world_behrmann projection
+        SR = getDefaultProjectionReference()->Clone();
+    }
+
+    // Assign the spatial reference to mOGRPoint.
+    // mOGRPoint does not take ownership of the spatialRef.
+    mOGRPoint.assignSpatialReference(SR);
+}
+
+std::shared_ptr<OGRSpatialReference> Point::getDefaultProjectionReference()
+{
+    // Initialize spatialRef with WGS84 if it hasn't been initialized yet
+    if (!spatialRef) {
+        Point::spatialRef = std::make_shared<OGRSpatialReference>();
+        const char *pszWKT = "PROJCS[\"World_Behrmann\","
+                             "GEOGCS[\"WGS 84\","
+                             "DATUM[\"WGS_1984\","
+                             "SPHEROID[\"WGS 84\",6378137,298.257223563,"
+                             "AUTHORITY[\"EPSG\",\"7030\"]],"
+                             "AUTHORITY[\"EPSG\",\"6326\"]],"
+                             "PRIMEM[\"Greenwich\",0],"
+                             "UNIT[\"Degree\",0.0174532925199433]],"
+                             "PROJECTION[\"Cylindrical_Equal_Area\"],"
+                             "PARAMETER[\"standard_parallel_1\",30],"
+                             "PARAMETER[\"central_meridian\",0],"
+                             "PARAMETER[\"false_easting\",0],"
+                             "PARAMETER[\"false_northing\",0],"
+                             "UNIT[\"metre\",1,"
+                             "AUTHORITY[\"EPSG\",\"9001\"]],"
+                             "AXIS[\"Easting\",EAST],"
+                             "AXIS[\"Northing\",NORTH],"
+                             "AUTHORITY[\"ESRI\",\"54017\"]]";
+
+        OGRErr err = Point::spatialRef->importFromWkt(pszWKT);
+        if (err != OGRERR_NONE) {
+            qFatal("Failed to set World Behrmann spatial reference");
+        }
+    }
+    return Point::spatialRef;
+}
+
+void Point::setDefaultProjectionReference(std::string wellknownCS)
+{
+    if (!Point::spatialRef) {
+        Point::spatialRef = std::make_shared<OGRSpatialReference>();
+    }
+
+    // Temporary spatial reference for validation
+    std::shared_ptr<OGRSpatialReference> tempRef =
+        std::make_shared<OGRSpatialReference>();
+    OGRErr err = tempRef->SetWellKnownGeogCS(wellknownCS.c_str());
+    if (err != OGRERR_NONE) {
+        // Exit the function on failure
+        qFatal("Failed to interpret the provided spatial reference: %",
+               qPrintable(QString::fromStdString(wellknownCS)));
+    }
+
+    // Check if the spatial reference is geodetic
+    if (!tempRef->IsProjected()) {
+        // Exit the function if not projected
+        qFatal("The provided spatial reference is not projected: %",
+               qPrintable(QString::fromStdString(wellknownCS)));
+    }
+
+    // If validation passed, assign the validated spatial
+    // reference to the class's static member
+    Point::spatialRef = tempRef;
+
+}
+
+OGRPoint Point::getGDALPoint() const { return mOGRPoint; }
+
+void Point::transformDatumTo(OGRSpatialReference *targetSR)
+{
+    if (targetSR && targetSR->IsProjected())
+    {
+        const OGRSpatialReference* currentSR = mOGRPoint.getSpatialReference();
+        if (currentSR && !currentSR->IsSame(targetSR)) {
+            OGRCoordinateTransformation* coordTransform =
+                OGRCreateCoordinateTransformation(currentSR, targetSR);
+            if (coordTransform) {
+                double x = mOGRPoint.getX(), y = mOGRPoint.getY();
+                if (coordTransform->Transform(1, &x, &y)) {
+                    // Update the internal OGRPoint coordinates
+                    mOGRPoint.setX(x);
+                    mOGRPoint.setY(y);
+
+                    // Update point's spatial reference to the target
+                    mOGRPoint.assignSpatialReference(targetSR);
+                }
+                OCTDestroyCoordinateTransformation(coordTransform);
+            }
+        }
+    }
+    else
+    {
+        qFatal("Target spatial reference is not projected!");
+    }
+}
+
+GPoint Point::reprojectTo(OGRSpatialReference *targetSR)
+{
+    // Ensure the target Spatial Reference is valid and is a projected CRS
+    if (!targetSR || !targetSR->IsGeographic()) {
+        qFatal("Target Spatial Reference "
+               "is not valid or not a geographic CRS.");
+    }
+
+    const OGRSpatialReference* currentSR = mOGRPoint.getSpatialReference();
+    if (currentSR == nullptr) {
+        qFatal("Current Spatial Reference is not set.");
+    }
+
+    // Create a coordinate transformation from the current
+    // geographic CRS to the target projected CRS
+    OGRCoordinateTransformation* coordTransform =
+        OGRCreateCoordinateTransformation(currentSR, targetSR);
+    if (!coordTransform) {
+        qFatal("Failed to create coordinate transformation.");
+    }
+
+    double x = mOGRPoint.getX();
+    double y = mOGRPoint.getY();
+
+    // Transform the point's coordinates from geographic to projected CRS
+    if (!coordTransform->Transform(1, &x, &y)) {
+        OCTDestroyCoordinateTransformation(coordTransform);
+        qFatal("Failed to transform point coordinates.");
+    }
+
+    OCTDestroyCoordinateTransformation(coordTransform);
+
+    // Create and return the transformed point.
+    return GPoint(units::angle::degree_t(x),
+                 units::angle::degree_t(y),
+                 mUserID, *targetSR);
+}
 
 // Parameterized constructor without user ID and index.
 Point::Point(units::length::meter_t xCoord,
              units::length::meter_t yCoord)
-    : mx(xCoord),  // Initialize mx with xCoord.
-    my(yCoord),  // Initialize my with yCoord.
-    mIsPort(false),  // Initialize mIsPort with false.
-    mDwellTime(units::time::second_t(0.0)),  // Initialize mDwellTime with 0.
-    userID("temporary point"),  // Initialize userID with a default value.
+    : mOGRPoint(xCoord.value(), yCoord.value()),  // Initialize the Coord.
+    mUserID("temporary point"),  // Initialize mIsPort with false.
+    mIsPort(false),  // Initialize userID with a default value.
     // Initialize index with the minimum possible value for unsigned int.
-    index(std::numeric_limits<unsigned int>::min())
-{}
+    mDwellTime(units::time::second_t(0.0))
+{
+    OGRSpatialReference* SR = getDefaultProjectionReference()->Clone();
+
+    // Assign the spatial reference to mOGRPoint.
+    // mOGRPoint does not take ownership of the spatialRef.
+    mOGRPoint.assignSpatialReference(SR);
+}
 
 // Destructor for the Point class.
 Point::~Point() {
@@ -59,13 +212,13 @@ Point::~Point() {
 // Getter function for x-coordinate.
 units::length::meter_t Point::x() const
 {
-    return mx;  // Return mx.
+    return units::length::meter_t(mOGRPoint.getX());  // Return mx.
 }
 
 // Getter function for y-coordinate.
 units::length::meter_t Point::y() const
 {
-    return my;  // Return my.
+    return units::length::meter_t(mOGRPoint.getY());  // Return my.
 }
 
 
@@ -73,30 +226,70 @@ units::length::meter_t Point::y() const
 bool Point::isValid()
 {
     // Return true if neither mx nor my is NaN.
-    return !(std::isnan(mx.value()) || std::isnan(my.value()));
+    return !(std::isnan(mOGRPoint.getX()) || std::isnan(mOGRPoint.getY()));
 }
 
 // Function to calculate the distance between the current point and endPoint.
-units::length::meter_t Point::distance(const Point &endPoint) const
+units::length::meter_t Point::distance(const Point &endPoint,
+                                       units::length::meter_t mapWidth) const
 {
     // Calculate the difference in x-coordinates.
-    units::length::meter_t dx = mx - endPoint.x();
+    units::length::meter_t dx = x() - endPoint.x();
     // Calculate the difference in y-coordinates.
-    units::length::meter_t dy = my - endPoint.y();
+    units::length::meter_t dy = y() - endPoint.y();
+
+    // Check if crossing the map boundary is shorter
+    if (!std::isnan(mapWidth.value()) && dx > mapWidth / 2)
+    {
+        dx = mapWidth - dx;
+    }
+
     // Calculate and return the Euclidean distance.
     return units::math::sqrt(units::math::pow<2>(dx) +
                              units::math::pow<2>(dy));
 }
 
 // Function to convert the point to a string representation.
-QString Point::toString()
+QString Point::toString() const
 {
+    QString str;
+    QString xStr = QString::number(mOGRPoint.getX(), 'f', 3); // Convert x-coordinate to string in decimal format
+    QString yStr = QString::number(mOGRPoint.getY(), 'f', 3); // Convert y-coordinate to string in decimal format
+
+
     // Format the string as "Point userID(x, y)".
-    QString str = QString("Point %1(%2, %3)")
-                      .arg(userID)  // Insert userID.
-                      .arg(mx.value())  // Insert x-coordinate.
-                      .arg(my.value());  // Insert y-coordinate.
+    if (mUserID.isEmpty() ||
+        mUserID == "temporary point")  // Format the string as "(x, y)".
+    {
+        str =
+            QString("(%1; %2)").arg(xStr, yStr);
+    }
+    else  // Format the string as "Point userID(x, y)".
+    {
+        str = QString("Point %1(%2; %3)").arg(mUserID, xStr, yStr);
+    }
     return str;  // Return the formatted string.
+}
+
+Point Point::pointAtDistanceAndHeading(units::length::meter_t distance,
+                                         units::angle::degree_t heading) const
+{
+    // Convert heading to radians
+    double headingRadians = heading.value() * (M_PI / 180.0);
+
+    // Calculate new x and y using trigonometry
+    // Heading is measured clockwise from the north
+    double newX = x().value() + distance.value() * cos(headingRadians);
+    double newY = y().value() + distance.value() * sin(headingRadians);
+
+    // Create a new GPoint with the calculated x, y.
+    // Assuming the spatial reference (SR) of the new point is the same
+    // as the current point.
+    const OGRSpatialReference* currentSR = mOGRPoint.getSpatialReference();
+
+    return Point(units::length::meter_t(newX),
+                 units::length::meter_t(newY),
+                 "NewPoint", *currentSR);
 }
 
 // Function to check if the point is a port.
@@ -121,20 +314,59 @@ void Point::MarkAsPort(units::time::second_t dwellTime)
 // Setter function for x-coordinate.
 void Point::setX(units::length::meter_t newX)
 {
-    mx = newX;  // Set mx to newX.
+    mOGRPoint.setX(newX.value());  // Set mx to newX.
 }
 
 // Setter function for y-coordinate.
 void Point::setY(units::length::meter_t newY)
 {
-    my = newY;  // Set my to newY.
+    mOGRPoint.setY(newY.value());  // Set my to newY.
+}
+
+Point Point::getMiddlePoint(const Point& endPoint)
+{
+    // Calculate the midpoint coordinates.
+    double midX = (x().value() + endPoint.x().value()) / 2.0;
+    double midY = (y().value() + endPoint.y().value()) / 2.0;
+
+    // Return the midpoint as a Point object.
+    return Point(units::length::meter_t(midX), units::length::meter_t(midY));
+}
+// // check if the point is within a bounding box
+// bool Point::isPointInsideBoundingBox(const Point& min_point,
+//                                      const Point& max_point) const
+// {
+//     return x() >= min_point.x() && x() <= max_point.x() &&
+//            y() >= min_point.y() && y() <= max_point.y();
+// }
+
+Point Point::operator*(const double scale) const
+{
+    return Point(x() * scale, y() * scale);
+}
+
+Point Point::operator-(const Point& other) const
+{
+    return Point(x() - other.x(), y() - other.y());
+}
+
+Point Point::operator+(const Point& other) const
+{
+    return Point(x() + other.x(), y() + other.y());
 }
 
 // Overloaded equality operator to compare two points.
 bool Point::operator==(const Point &other) const
 {
     // Return true if both x and y coordinates are the same.
-    return mx == other.x() && my == other.y();
+    return x() == other.x() && y() == other.y();
+}
+
+bool Point::isExactlyEqual(const Point& other) const
+{
+    return this==&other && mUserID == other.mUserID &&
+           mIsPort == other.mIsPort &&
+           mDwellTime == other.mDwellTime;
 }
 
 // Hash function for the Point class.
@@ -143,4 +375,128 @@ std::size_t std::hash<Point>::operator()(const Point &p) const
     // Calculate the hash value using the x and y coordinates.
     return std::hash<long double>()(p.x().value()) ^
            std::hash<long double>()(p.y().value());
+}
+void Point::serialize(std::ostream& out) const
+{
+    if (!out) {
+        throw std::runtime_error("Output stream is not ready for writing.");
+    }
+
+    // Serialize x-coordinate
+    std::uint64_t xNet =
+        qToBigEndian(std::bit_cast<std::uint64_t>(x().value()));
+    out.write(reinterpret_cast<const char*>(&xNet), sizeof(xNet));
+
+    // Serialize y-coordinate
+    std::uint64_t yNet =
+        qToBigEndian(std::bit_cast<std::uint64_t>(y().value()));
+    out.write(reinterpret_cast<const char*>(&yNet), sizeof(yNet));
+
+    // Serialize mUserID as string length followed by string data
+    std::string userIDStr = mUserID.toStdString();
+    std::uint64_t userIDLen =
+        qToBigEndian(static_cast<std::uint64_t>(userIDStr.size()));
+    out.write(reinterpret_cast<const char*>(&userIDLen),
+              sizeof(userIDLen));
+    out.write(userIDStr.c_str(), userIDStr.size());
+
+    // Serialize mIsPort
+    out.write(reinterpret_cast<const char*>(&mIsPort),
+              sizeof(mIsPort));
+
+    // Serialize mDwellTime
+    std::uint64_t dwellTimeNet =
+        qToBigEndian(std::bit_cast<std::uint64_t>(mDwellTime.value()));
+    out.write(reinterpret_cast<const char*>(&dwellTimeNet),
+              sizeof(dwellTimeNet));
+
+    // Check for write failures
+    if (!out) {
+        throw std::runtime_error("Failed to write point"
+                                 " data to output stream.");
+    }
+}
+
+void Point::deserialize(std::istream& in)
+{
+    if (!in)
+    {
+        throw std::runtime_error("Input stream is not ready for reading.");
+    }
+
+    // Deserialize x-coordinate
+    std::uint64_t xNet;
+    in.read(reinterpret_cast<char*>(&xNet), sizeof(xNet));
+    if (!in)
+    {
+        throw std::runtime_error("Failed to read x-coordinate"
+                                 " from input stream.");
+    }
+    mOGRPoint.setX(qFromBigEndian(std::bit_cast<double>(xNet)));
+
+    // Deserialize y-coordinate
+    std::uint64_t yNet;
+    in.read(reinterpret_cast<char*>(&yNet), sizeof(yNet));
+    if (!in) {
+        throw std::runtime_error("Failed to read y-coordinate "
+                                 "from input stream.");
+    }
+    mOGRPoint.setY(qFromBigEndian(std::bit_cast<double>(yNet)));
+
+    // Deserialize userID
+    std::uint64_t userIDLen;
+    in.read(reinterpret_cast<char*>(&userIDLen), sizeof(userIDLen));
+    userIDLen = qFromBigEndian(userIDLen);
+    std::string userIDStr(userIDLen, '\0');
+    in.read(&userIDStr[0], userIDLen);
+    if (!in)
+    {
+        throw std::runtime_error("Failed to read userID "
+                                 "from input stream.");
+    }
+    mUserID = QString::fromStdString(userIDStr);
+
+    // Deserialize mIsPort
+    in.read(reinterpret_cast<char*>(&mIsPort), sizeof(mIsPort));
+    if (!in)
+    {
+        throw std::runtime_error("Failed to read mIsPort "
+                                 "flag from input stream.");
+    }
+
+    // Deserialize mDwellTime
+    std::uint64_t dwellTimeNet;
+    in.read(reinterpret_cast<char*>(&dwellTimeNet),
+            sizeof(dwellTimeNet));
+    if (!in) {
+        throw std::runtime_error("Failed to read dwell "
+                                 "time from input stream.");
+    }
+    mDwellTime = units::time::second_t(
+        qFromBigEndian(std::bit_cast<double>(dwellTimeNet)));
+}
+
+// Implementation of the nested Hash structure
+std::size_t Point::Hash::operator()(const std::shared_ptr<Point>& p) const
+{
+    if (!p) return 0; // Handle null pointers
+
+    auto x_value = p->x().value();
+    auto y_value = p->y().value();
+
+    return std::hash<decltype(x_value)>()(x_value) ^
+           std::hash<decltype(y_value)>()(y_value);
+}
+
+// Implementation of the nested Equal structure
+bool Point::Equal::operator()(const std::shared_ptr<Point>& lhs,
+                              const std::shared_ptr<Point>& rhs) const
+{
+    if (!lhs || !rhs) return false; // Handle null pointers
+    return *lhs == *rhs;
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& point)
+{
+    return os << point.toString().toStdString();
 }
--- src/ShipNetSim/network/point.h
+++ src/ShipNetSim/network/point.h
@@ -16,11 +16,16 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include <gdal.h>
+#include <ogr_geometry.h>
+#include <ogr_spatialref.h>
 #include "basegeometry.h"
-#include <string>
 #include <functional>
 #include <cmath>
 #include "../../third_party/units/units.h"
+
+
+class GPoint; // Forward declaration
 
 /**
  * @class Point
@@ -53,10 +58,21 @@ public:
      * @param xCoord The x-coordinate of the point.
      * @param yCoord The y-coordinate of the point.
      * @param ID The unique ID of the point.
-     * @param index The index of the point.
      */
     Point(units::length::meter_t xCoord, units::length::meter_t yCoord,
-          QString ID, unsigned int index);
+          QString ID, OGRSpatialReference crc = OGRSpatialReference());
+
+    OGRPoint getGDALPoint() const;
+
+    Point pointAtDistanceAndHeading(units::length::meter_t distance,
+                                    units::angle::degree_t heading) const;
+
+    void transformDatumTo(OGRSpatialReference* targetSR);
+    GPoint reprojectTo(OGRSpatialReference* targetSR);
+
+    static std::shared_ptr<OGRSpatialReference> getDefaultProjectionReference();
+
+    static void setDefaultProjectionReference(std::string wellknownCS);
 
     /**
      * @brief Parameterized constructor.
@@ -89,14 +105,16 @@ public:
      * @return Distance to the other point.
      */
     [[nodiscard]] units::length::meter_t
-    distance(const Point &endPoint) const;
+    distance(const Point &endPoint,
+             units::length::meter_t mapWidth =
+             units::length::meter_t(std::nan("No Value"))) const;
 
     /**
      * @brief Get a string representation of the point.
      *
      * @return A string representing the point.
      */
-    [[nodiscard]] QString toString() override;
+    [[nodiscard]] QString toString() const override;
 
     /**
      * @brief Equality operator.
@@ -107,6 +125,18 @@ public:
      * @return True if equal, false otherwise.
      */
     [[nodiscard]] bool operator==(const Point &other) const;
+
+    [[nodiscard]] Point operator*(const double scale) const;
+    [[nodiscard]] Point operator+(const Point& other) const;
+    [[nodiscard]] Point operator-(const Point& other) const;
+
+    /**
+     * @brief Check if two points are exactly equal.
+     * this includes all parameters.
+     * @param other The other point.
+     * @return True if exactly equal, false otherwise.
+     */
+    [[nodiscard]] bool isExactlyEqual(const Point& other) const;
 
     /**
      * @brief Get the x-coordinate of the point.
@@ -157,14 +187,96 @@ public:
      */
     void setY(units::length::meter_t newY);
 
+    /**
+     * @brief get the middle point between the current point and an end point.
+     * @param endPoint The end point of the segment.
+     * @return the middle point between the current point and the end point.
+     */
+    [[nodiscard]] Point getMiddlePoint(const Point& endPoint);
+
+    /**
+     * @brief Serialize the point to a binary format and write it
+     *          to an output stream.
+     *
+     * This function serializes the point class data to a binary format.
+     * The serialized data is written to the provided outptut stream.
+     * The serialization format is designed for efficient storage and
+     * is not hyman-readable. This function is typically used to save
+     * the state of the point to a file or transmit it over a network.
+     *
+     * @param out The output stream to which the point is serialized.
+     *              This stream should be opened in binary mode to
+     *              correctly handle the binary data.
+     *
+     * Usage Example:
+     *      std::ofstream outFile("point.bin", std::ios::binary);
+     *      if (outFile.is_open()) {
+     *          point.serialize(outFile);
+     *          outFile.close();
+     *      }
+     */
+    void serialize(std::ostream& out) const;
+
+    /**
+     * @brief Deserialize the point from a binary formate read from
+     *          an input stream.
+     *
+     * This function reads binary data from the provided input stream
+     * and reconstructs the point. The binary format should match the
+     * format produced by the serialize function.
+     * This function is typically used to load a point from a file
+     * or received it from a network transmission. The existing content
+     * of the Point is clearned before deserialization. If the binary
+     * data is corrupted or improperly formatted,
+     * The behavior of this function is undefined, and the resulting
+     * Point may be incomplete or invalid.
+     *
+     * @param in The input stream from which the Point is deserialized.
+     *              This stream should be opened in binary mode to
+     *              coorectly handle the binary data.
+     *
+     * Usage Example:
+     *     std::ifstream inFile("point.bin", std::ios::binary);
+     *     if (inFile.is_open()) {
+     *         point.deserialize(inFile);
+     *         inFile.close();
+     *     }
+     */
+    void deserialize(std::istream& in);
+
+    // Nested custom hash function for Point
+    struct Hash {
+        std::size_t operator()(
+            const std::shared_ptr<Point>& p) const;
+    };
+
+    // Nested custom equality function for Point
+    struct Equal {
+        bool operator()(const std::shared_ptr<Point>& lhs,
+                        const std::shared_ptr<Point>& rhs) const;
+    };
+
+    /**
+     * @brief Stream insertion operator for Point.
+     *
+     * Outputs a string representation of the Point to an output stream.
+     *
+     * @param os The output stream.
+     * @param point The Point object to be outputted.
+     * @return Reference to the output stream.
+     */
+    friend std::ostream& operator<<(std::ostream& os, const Point& point);
+
+
 private:
-    units::length::meter_t mx; ///< The x-coordinate of the point.
-    units::length::meter_t my; ///< The y-coordinate of the point.
-    QString userID; ///< The unique ID of the point.
-    unsigned int index; ///< The index of the point.
+    OGRPoint mOGRPoint; ///< GDAL Point definition
+    QString mUserID; ///< The unique ID of the point.
     bool mIsPort; ///< Flag indicating if the point is a port.
     units::time::second_t
         mDwellTime; ///< The dwell time if the point is a port.
+
+     /// The default Coordinate system
+    static std::shared_ptr<OGRSpatialReference> spatialRef;
 };
 
 /**
