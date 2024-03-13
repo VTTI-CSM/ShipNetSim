3a35f98b37217a82c33f588822a0b38c3d4011f5
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-03-12 22:21:17 -0400

    load ships default values

---
+++ src/ShipNetSim/ship/defaults.h
@@ -0,0 +1,26 @@
+/**
+ * @file defaults.h
+ * @brief This file containes the defaults values for the simulator.
+ *
+ * This class holds the default values for all the simulator variables.
+ *
+ * @author Ahmed Aredah
+ * @date 11.6.2023
+ */
+
+#ifndef DEFAULTS_H
+#define DEFAULTS_H
+
+#include "../../third_party/units/units.h"
+#include <qstring>
+
+namespace Defaults
+{
+    units::volume::liter_t TankSize      = units::volume::liter_t(11356235.35);
+    double TankInitialCapacityPercentage = 0.9;
+    double TankDepthOfDischarge          = 0.9;
+
+    int PropellerCountPerShip            = 1;
+    int EngineCountPerPropeller          = 1;
+}
+#endif // DEFAULTS_H
