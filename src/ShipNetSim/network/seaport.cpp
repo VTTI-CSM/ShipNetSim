9e602cc3032d38a9be9b1dc831a89ff6d8551eae
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-03-12 22:19:02 -0400

    define seaports

---
+++ src/ShipNetSim/network/seaport.cpp
@@ -0,0 +1,30 @@
+#include "seaport.h"
+
+SeaPort::SeaPort(GPoint newCoordinate) : mPortCoordinate(newCoordinate) {}
+
+void SeaPort::setPortCoordinate(GPoint newCoordinate) {
+    mPortCoordinate = newCoordinate;
+}
+
+void SeaPort::setCountryName(QString country) { mPortCountry = country; }
+void SeaPort::setPortName(QString portName) { mPortName = portName; }
+void SeaPort::setPortCode(QString portCode) { mPortCode = portCode; }
+
+void SeaPort::setClosestPointOnWaterPolygon(
+    std::shared_ptr<GPoint> newCoordinate)
+{
+    mClosestPointOnWaterPolygon = newCoordinate;
+    if (!mClosestPointOnWaterPolygon->isPort())
+    {
+        mClosestPointOnWaterPolygon->MarkAsPort(units::time::second_t(0.0));
+    }
+}
+
+
+GPoint SeaPort::getPortCoordinate() { return mPortCoordinate; }
+std::shared_ptr<GPoint> SeaPort::getClosestPointOnWaterPolygon() {
+    return mClosestPointOnWaterPolygon;
+}
+QString SeaPort::getCountryName() { return mPortCountry; }
+QString SeaPort::getPortName() { return mPortName; }
+QString SeaPort::getPortCode() { return mPortCode; }
---
+++ src/ShipNetSim/network/seaport.h
@@ -0,0 +1,34 @@
+#ifndef SEAPORT_H
+#define SEAPORT_H
+
+#include "gpoint.h"
+#include <QString>
+
+class SeaPort
+{
+public:
+    // SeaPort();
+
+    SeaPort(GPoint newCoordinate);
+
+    void setPortCoordinate(GPoint newCoordinate);
+    void setClosestPointOnWaterPolygon(std::shared_ptr<GPoint> newCoordinate);
+    void setCountryName(QString country);
+    void setPortName(QString portName);
+    void setPortCode(QString portCode);
+
+    GPoint getPortCoordinate();
+    std::shared_ptr<GPoint> getClosestPointOnWaterPolygon();
+    QString getCountryName();
+    QString getPortName();
+    QString getPortCode();
+
+private:
+    QString mPortCountry;
+    QString mPortName;
+    QString mPortCode;
+    GPoint mPortCoordinate;
+    std::shared_ptr<GPoint> mClosestPointOnWaterPolygon;
+};
+
+#endif // SEAPORT_H
