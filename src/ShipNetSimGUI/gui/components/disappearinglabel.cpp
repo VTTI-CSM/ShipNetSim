58548f979413205ca561f230de30dc31af48a613
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-08-11 12:29:19 -0400

    add color

--- src/ShipNetSimGUI/gui/components/disappearinglabel.cpp
+++ src/ShipNetSimGUI/gui/components/disappearinglabel.cpp
@@ -8,10 +8,13 @@ DisappearingLabel::DisappearingLabel(QWidget *parent) :
     connect(&m_timer, &QTimer::timeout, this, &DisappearingLabel::clearText);
 }
 
-void DisappearingLabel::setTextWithTimeout(const QString &text, int timeoutMs)
+void DisappearingLabel::setTextWithTimeout(const QString &text,
+                                           int timeoutMs,
+                                           const QColor &color)
 {
     // Set the label text and show it
     setText(text);
+    setStyleSheet(QString("color: %1").arg(color.name()));
     show();
 
     // Start the timer to clear the label text after the specified timeout
