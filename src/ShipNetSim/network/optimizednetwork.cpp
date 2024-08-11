0103f89161d734cb26feb458b57ffbd71a60db64
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-08-11 12:24:22 -0400

    change status code to status description

--- src/ShipNetSim/network/optimizednetwork.cpp
+++ src/ShipNetSim/network/optimizednetwork.cpp
@@ -580,6 +580,7 @@ QVector<std::shared_ptr<SeaPort> > OptimizedNetwork::readSeaPorts(const char* fi
             const char* country = feature->GetFieldAsString("Country");
             const char* nameWoDiac = feature->GetFieldAsString("NameWoDiac");
             const char* locode = feature->GetFieldAsString("LOCODE");
+            QString qStatus = QString::fromUtf8(status).toUpper();
             OGRGeometry* geometry = feature->GetGeometryRef();
 
             if (geometry != nullptr &&
@@ -594,6 +595,48 @@ QVector<std::shared_ptr<SeaPort> > OptimizedNetwork::readSeaPorts(const char* fi
                     sp.setCountryName(country);
                     sp.setPortCode(locode);
                     sp.setPortName(nameWoDiac);
+                    sp.setHasRailTerminal(function[1] != '-');
+                    sp.setHasRoadTerminal(function[2] != '-');
+
+
+                    QString statusText;
+
+                    if (qStatus == "AA") {
+                        statusText = "Approved by competent national "
+                                     "government agency";
+                    } else if (qStatus == "AC") {
+                        statusText = "Approved by Customs Authority";
+                    } else if (qStatus == "AF") {
+                        statusText = "Approved by national facilitation body";
+                    } else if (qStatus == "AI") {
+                        statusText = "Code adopted by international "
+                                     "organisation (IATA or ECLAC)";
+                    } else if (qStatus == "AS") {
+                        statusText = "Approved by national standardisation "
+                                     "body";
+                    } else if (qStatus == "RL") {
+                        statusText = "Recognised location - Existence and "
+                                     "representation of location name "
+                                     "confirmed by check against nominated "
+                                     "gazetteer or other reference work";
+                    } else if (qStatus == "RN") {
+                        statusText = "Request from credible national sources "
+                                     "for locations in their own country";
+                    } else if (qStatus == "RQ") {
+                        statusText = "Request under consideration";
+                    } else if (qStatus == "RR") {
+                        statusText = "Request rejected";
+                    } else if (qStatus == "QQ") {
+                        statusText = "Original entry not verified since date "
+                                     "indicated";
+                    } else if (qStatus == "XX") {
+                        statusText = "Entry that will be removed from the "
+                                     "next issue of UN/LOCODE";
+                    } else {
+                        statusText = "Unknown status code";
+                    }
+
+                    sp.setStatusOfEntry(statusText);
                 } catch (std::exception& e)
                 {
                     qWarning(e.what());
