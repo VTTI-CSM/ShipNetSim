a40e6edd7965d5b8895b9f9ff6d0bc7f59d53a60
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2024-11-06 12:02:26 -0500

    fix reference to data folder

--- src/ShipNetSimCore/network/networkdefaults.h
+++ src/ShipNetSimCore/network/networkdefaults.h
@@ -1,6 +1,7 @@
 #ifndef NETWORKDEFAULTS_H
 #define NETWORKDEFAULTS_H
 
+#include "qdir.h"
 #include <QVector>
 #include <QString>
 
@@ -9,81 +10,68 @@ namespace ShipNetSimCore
 namespace NetworkDefaults
 {
 
-static const QVector<QString>& worldNetworkLocation() {
+static const QVector<QString>& worldNetworkLocation(QString baseDirectory) {
     static QVector<QString> v
         {
-        "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-        "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/ne_110m_ocean.shp"
+            QDir(baseDirectory).filePath("ne_110m_ocean.shp")
         };
     return v;
 };
 
-static const QVector<QString>& SalinityTiffLocations() {
+static const QVector<QString>& SalinityTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "salinity.tif"
+            QDir(baseDirectory).filePath("salinity.tif")
         };
     return v;
 };
 
-static const QVector<QString>& WaveHeightTiffLocations() {
+static const QVector<QString>& WaveHeightTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "waveHeight.tif"
+            QDir(baseDirectory).filePath("waveHeight.tif")
         };
     return v;
 };
 
-static const QVector<QString>& wavePeriodTiffLocations() {
+static const QVector<QString>& wavePeriodTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "wavePeriod.tif"
+            QDir(baseDirectory).filePath("wavePeriod.tif")
         };
     return v;
 };
 
-static const QVector<QString>& windSpeedNorthTiffLocations() {
+static const QVector<QString>&
+windSpeedNorthTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "WindSpeedNorth.tif"
+            QDir(baseDirectory).filePath("WindSpeedNorth.tif")
         };
     return v;
 };
 
-static const QVector<QString>& windSpeedEastTiffLocations() {
+static const QVector<QString>&
+windSpeedEastTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "WindSpeedEast.tif"
+            QDir(baseDirectory).filePath("WindSpeedEast.tif")
         };
     return v;
 };
 
-static const QVector<QString>& waterDepthTiffLocations() {
+static const QVector<QString>& waterDepthTiffLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "WaterDepth.tif"
+            QDir(baseDirectory).filePath("WaterDepth.tif")
         };
     return v;
 };
 
-static const QVector<QString>& seaPortsLocations() {
+static const QVector<QString>& seaPortsLocations(QString baseDirectory) {
     static QVector<QString> v
         {
-            "D:/OneDrive - Virginia Tech/03.Work/02.VTTI/02.ResearchWork/"
-            "04.ShipModelling/01.Code/00.CPP/ShipNetSim/src/data/"
-            "ports.geojson"
+            QDir(baseDirectory).filePath("ports.geojson")
         };
     return v;
 }
