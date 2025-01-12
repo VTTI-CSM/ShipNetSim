f179e63f2d0f3cf274f8d27d54508eee98e2011a
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2025-01-11 20:05:27 -0500

    add a thread worker for loading simulator in a qthread

---
+++ src/ShipNetSimCore/simulatorworker.cpp
@@ -0,0 +1,28 @@
+#include "simulatorapi.h"
+#include "simulatorworker.h"
+
+SimulatorWorker::SimulatorWorker(QObject *parent)
+    : QObject{parent}
+{}
+
+void SimulatorWorker::setupSimulator(
+    APIData& apiData,
+    QVector<std::shared_ptr<ShipNetSimCore::Ship>>& shipList,
+    units::time::second_t timeStep, bool isExternallyControlled)
+{
+    qDebug() << "Creating simulator inside thread:"
+             << QThread::currentThread();
+
+    // ✅ Create Simulator inside the worker thread
+    apiData.simulator = new ShipNetSimCore::Simulator(
+        apiData.network, shipList, timeStep, isExternallyControlled);
+
+    // Store the ship list in APIData for future reference
+    apiData.ships.clear();
+    for (const auto& ship : shipList) {
+        apiData.ships[ship->getUserID()] = ship;
+    }
+
+    qDebug() << "Simulator successfully created inside thread: "
+             << QThread::currentThread();
+}
