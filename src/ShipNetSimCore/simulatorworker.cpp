bc2bcbf8b7e8b1c9063cb7808707a50b9564356b
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2025-01-26 21:07:53 -0500

    refactor(simulatorworker): fix indentation for better readability
    Corrects the indentation of the code block within the `setupSimulator` method to improve readability and maintain coding standards.

--- src/ShipNetSimCore/simulatorworker.cpp
+++ src/ShipNetSimCore/simulatorworker.cpp
@@ -14,18 +14,18 @@ void SimulatorWorker::setupSimulator(
              << QThread::currentThread();
 
     try {
-    // Create Simulator inside the worker thread
-    apiData.simulator = new ShipNetSimCore::Simulator(
-        apiData.network, shipList, timeStep, isExternallyControlled);
+        // Create Simulator inside the worker thread
+        apiData.simulator = new ShipNetSimCore::Simulator(
+            apiData.network, shipList, timeStep, isExternallyControlled);
 
-    // Store the ship list in APIData for future reference
-    apiData.ships.clear();
-    for (const auto& ship : shipList) {
-        apiData.ships[ship->getUserID()] = ship;
-    }
+        // Store the ship list in APIData for future reference
+        apiData.ships.clear();
+        for (const auto& ship : shipList) {
+            apiData.ships[ship->getUserID()] = ship;
+        }
 
-    qDebug() << "Simulator successfully created inside thread: "
-             << QThread::currentThread();
+        qDebug() << "Simulator successfully created inside thread: "
+                 << QThread::currentThread();
     }
     catch (std::exception &e) {
         emit errorOccurred(QString("Error: Error in setting the "
